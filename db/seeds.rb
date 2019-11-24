0d9d26877563733041a5b48a2f2ff689c86cae8a
Karmar Barrett <karmar.barrett97@gmail.com>
2019-11-24 17:40:11 +1100

    new seed and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,12 @@ ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.
 
 ingredients['drinks'].each { |hash| Ingredient.create(name: hash['strIngredient1']) }
 
-10.times do
-  Cocktail.create(name: Faker::Coffee.blend_name)
-end
+Cocktail.create(name: "Sweet Poison", photo: "https://hips.hearstapps.com/del.h-cdn.co/assets/15/38/1442432590-delish-halloween-cocktails-sweet-poison.png")
+Cocktail.create(name: "Hot Mama", photo: "https://www.kitchengonerogue.com/wp-content/uploads/2017/05/sweet-hot-mama-cocktail_kitchengonerogue-600x900.jpg")
+Cocktail.create(name: "Thyme to Delight", photo: "https://stevethebartender.com.au/wp-content/uploads/2016/02/strawberry-gin-cocktail.jpg")
+Cocktail.create(name: "Hard Shandy", photo: "https://www.tullamoredew.com/assets/uploads/cocktails/18-hardshandy-large.jpg")
+Cocktail.create(name: "Negroni", photo: "https://s3-eu-west-1.amazonaws.com/beefeater.com/stage/DrinkToMarket/4716/drink/3/drink_500x753.jpeg")
+Cocktail.create(name: "Fall from the Tree", photo: "https://cdn.liquor.com/wp-content/uploads/2018/08/31124049/3-Prebatched-Cocktails-for-your-Football-Parties-720x720-article.jpg")
+Cocktail.create(name: "The Phoenix", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmhCb8WyCyUh9h6DCzjNkEKpfDXDwx5r0TdyFoSCzNQyOMlEaDAg&s")
 
 p "Finished seeds."
